246a355ec80cc509fe6d981f428a39d5d1085806
hanul93 <hanul93@gmail.com>
2013-05-16 07:22:50 +0900

    use MD5 hash

--- vaccine.py
+++ vaccine.py
@@ -1,13 +1,20 @@
 # -*- coding:utf-8 -*-
 # Made by Kei Choi(hanul93@gmail.com)
 
+import hashlib
+
 # Open a file
 fp = open('eicar.txt', 'rb')
 fbuf = fp.read()
 fp.close()
 
+# Create MD5 hash
+md5 = hashlib.md5()
+md5.update(fbuf)
+fmd5 = md5.hexdigest()
+
 # Detection of malware
-if fbuf[0:3] == 'X5O' :
+if fmd5 == '44d88612fea8a8f36de82e1278abb02f' :
     print 'Found Virus'
 else :
     print 'Not Found'
