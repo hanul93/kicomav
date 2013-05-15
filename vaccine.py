7ada68e78fc333415c067f6e5ef7666e0c2e57d3
hanul93 <hanul93@gmail.com>
2013-05-16 07:27:23 +0900

    Separate cure module from vaccine.py

--- vaccine.py
+++ vaccine.py
@@ -4,7 +4,22 @@
 import hashlib
 import os
 import sys
+import re
+import imp
 
+#-----------------------------------------------------------------
+# CheckRule(rule, s)
+#-----------------------------------------------------------------
+def CheckRule(rule, s) :
+	ret = re.match(rule, s)
+	if  ret == None : return None
+	if len(ret.group()) != len(s) : return None
+	return True
+		
+
+rule_size = '[0-9]+'       # rule of file size
+rule_md5 = '[0-9a-f]{32}'  # rule of MD5 hash
+	
 # Malware Patterns
 VirusDB = []
 fp = open('virus.db')
@@ -26,6 +41,14 @@ VirusFound = -1
 for i in range(len(VirusDB)) :
 	# Read a pattern of Malware
 	vdb = VirusDB[i].split(':')
+
+	# Check file size & MD5 hash
+	r1 = CheckRule(rule_size, vdb[1])
+	r2 = CheckRule(rule_md5, vdb[2])
+
+	if r1 == None or r2 == None:
+		continue
+
 	VirusName = vdb[0] # Malware name
 	VirusSize = vdb[1] # Malware file size
 	VirusMD5  = vdb[2] # Malware MD5 hash
@@ -51,8 +74,15 @@ for i in range(len(VirusDB)) :
 
 if VirusFound != -1 : # Found a Malware
 	vdb = VirusDB[VirusFound].split(':')
-	VirusName = vdb[0] # Malware name
+	VirusName    = vdb[0] # Malware name
+	VirusCureFn  = vdb[3] # name of Malware cure function 
 	print '[%s] : Found %s Virus -> Repaired' % (fname, VirusName)
-	os.remove(fname) # Remove malware
+	
+	# Remove malware
+	m = 'curemod'                                   # pyc name
+	f, filename, desc = imp.find_module(m, [''])
+	module = imp.load_module(m, f, filename, desc)  # load module
+	cmd = 'module.%s(fname)' % VirusCureFn
+	exec cmd
 else :
 	print 'Not Found'
