7b62a27d9555dfbd6f641909554ccd9fcb89977d
hanul93 <hanul93@gmail.com>
2017-11-28 13:40:30 +0900

    Added the scan function of Trojan.PDF.Generic

--- Engine/plugins/pdf.py
+++ Engine/plugins/pdf.py
@@ -3,7 +3,9 @@
 
 
 import re
+import os
 import zlib
+import kernel
 
 
 # -------------------------------------------------------------------------
@@ -36,6 +38,9 @@ class KavMain:
         pat = '/Filter\s*/(\w+)'
         self.p_pdf_filter = re.compile(pat, re.IGNORECASE)
 
+        pat = r'this\.exportDataObject.+?cName:.+?nLaunch'
+        self.p_pdf_trojan_js = re.compile(pat)
+
         return 0  # 플러그인 엔진 초기화 성공
 
     # ---------------------------------------------------------------------
@@ -55,9 +60,10 @@ class KavMain:
         info = dict()  # 사전형 변수 선언
 
         info['author'] = 'Kei Choi'  # 제작자
-        info['version'] = '1.0'  # 버전
+        info['version'] = '1.1'  # 버전
         info['title'] = 'PDF Engine'  # 엔진 설명
         info['kmd_name'] = 'pdf'  # 엔진 파일 이름
+        info['sig_num'] = 1  # 진단/치료 가능한 악성코드 수
 
         return info
 
@@ -84,6 +90,59 @@ class KavMain:
         return None
 
     # ---------------------------------------------------------------------
+    # scan(self, filehandle, filename, fileformat)
+    # 악성코드를 검사한다.
+    # 입력값 : filehandle  - 파일 핸들
+    #         filename    - 파일 이름
+    #         fileformat  - 파일 포맷
+    #         filename_ex - 파일 이름 (압축 내부 파일 이름)
+    # 리턴값 : (악성코드 발견 여부, 악성코드 이름, 악성코드 ID) 등등
+    # ---------------------------------------------------------------------
+    def scan(self, filehandle, filename, fileformat, filename_ex):  # 악성코드 검사
+        try:
+            # 미리 분석된 파일 포맷중에 PDF 포맷이 있는가?
+            if 'ff_pdf' in fileformat:
+                mm = filehandle
+
+                if self.p_pdf_trojan_js.search(mm):
+                    return True, 'Trojan.PDF.Generic', 0, kernel.INFECTED
+        except IOError:
+            pass
+
+        # 악성코드를 발견하지 못했음을 리턴한다.
+        return False, '', -1, kernel.NOT_FOUND
+
+    # ---------------------------------------------------------------------
+    # disinfect(self, filename, malware_id)
+    # 악성코드를 치료한다.
+    # 입력값 : filename    - 파일 이름
+    #        : malware_id - 치료할 악성코드 ID
+    # 리턴값 : 악성코드 치료 여부
+    # ---------------------------------------------------------------------
+    def disinfect(self, filename, malware_id):  # 악성코드 치료
+        try:
+            # 악성코드 진단 결과에서 받은 ID 값이 0인가?
+            if malware_id == 0:
+                os.remove(filename)  # 파일 삭제
+                return True  # 치료 완료 리턴
+        except IOError:
+            pass
+
+        return False  # 치료 실패 리턴
+
+    # ---------------------------------------------------------------------
+    # listvirus(self)
+    # 진단/치료 가능한 악성코드의 리스트를 알려준다.
+    # 리턴값 : 악성코드 리스트
+    # ---------------------------------------------------------------------
+    def listvirus(self):  # 진단 가능한 악성코드 리스트
+        vlist = list()  # 리스트형 변수 선언
+
+        vlist.append('Trojan.PDF.Generic')  # 진단/치료하는 악성코드 이름 등록
+
+        return vlist
+
+    # ---------------------------------------------------------------------
     # arclist(self, filename, fileformat)
     # 압축 파일 내부의 파일 목록을 얻는다.
     # 입력값 : filename   - 파일 이름
@@ -95,8 +154,6 @@ class KavMain:
 
         # 미리 분석된 파일 포맷중에 PDF 포맷이 있는가?
         if 'ff_pdf' in fileformat:
-            buf = ''
-
             try:
                 with open(filename, 'rb') as fp:
                     buf = fp.read()
