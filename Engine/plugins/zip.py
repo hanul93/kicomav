bf873e35ade1bc93ab731b8b2ead4705b6c2d3a8
hanul93 <hanul93@gmail.com>
2018-01-25 11:55:07 +0900

    Added a new ZipFile for only KicomAV

--- Engine/plugins/zip.py
+++ Engine/plugins/zip.py
@@ -2,8 +2,245 @@
 # Author: Kei Choi(hanul93@gmail.com)
 
 
+import struct
+import zlib
+import os
+
 import zipfile
 import kernel
+
+
+# 참소 : https://users.cs.jmu.edu/buchhofp/forensics/formats/pkzip.html
+
+# ---------------------------------------------------------------------
+# 엔진 오류 메시지를 정의
+# ---------------------------------------------------------------------
+class BadZipTagError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class NeedPasswordError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+# ---------------------------------------------------------------------
+# New ZipFile을 위한 구조체 정의
+# ---------------------------------------------------------------------
+struct_zipfilerecord = '<5H3L2H'  # PK\x03\x04
+size_struct_zipfilerecord = struct.calcsize(struct_zipfilerecord)
+
+struct_zipdirentry = '<6H3L5H2L'  # PK\x01\x02
+size_struct_zipdirentry = struct.calcsize(struct_zipdirentry)
+
+struct_zipdigitalsig = '<H'  # PK\x05\x05
+size_struct_zipdigitalsig = struct.calcsize(struct_zipdigitalsig)
+
+struct_zipdatadescr = '<3H'  # PK\x07\x08
+size_struct_zipdatadescr = struct.calcsize(struct_zipdatadescr)
+
+struct_zipendlocator = '<4H2LH'  # PK\x05\x06
+size_struct_zipendlocator = struct.calcsize(struct_zipendlocator)
+
+
+# ---------------------------------------------------------------------
+# COMP_TYPE 정의
+# ---------------------------------------------------------------------
+COMP_STORED = 0
+COMP_SHRUNK = 1
+COMP_REDUCED1 = 2
+COMP_REDUCED2 = 3
+COMP_REDUCED3 = 4
+COMP_REDUCED4 = 5
+COMP_IMPLODED = 6
+COMP_TOKEN = 7
+COMP_DEFLATE = 8
+COMP_DEFLATE64 = 9
+
+
+# ---------------------------------------------------------------------
+# New ZipFile 클래스
+# ---------------------------------------------------------------------
+class NZipFile:
+    def __init__(self, fname):
+        self.fname = fname
+        self.fp = None
+        self.fsize = 0
+        self.zipsize = 0
+
+        self.zipfilerecord = []
+        self.zipdirentry = []
+        self.zipdigitalsig = []
+        self.zipdatadescr = []
+        self.zipendlocator = []
+
+    def __del__(self):
+        if self.fp:
+            self.close()
+
+    def close(self):
+        if self.fp:
+            self.fp.close()
+            self.fp = None
+
+    def get_zipsize(self):
+        return self.zipsize
+
+    def namelist(self):
+        flist = []
+
+        for fr in self.zipfilerecord:
+            flist.append(fr[10])
+
+        return flist
+
+    def read(self, fname):
+        for fr in self.zipfilerecord:
+            if fr[10] == fname:
+                if fr[1] & 1 == 1:  # falgs - Password 필요
+                    raise NeedPasswordError('%s' % fname)
+                else:
+                    if fr[2] == COMP_STORED:  # COMTypes
+                        return fr[12]
+                    elif fr[2] == COMP_DEFLATE:
+                        return zlib.decompress(fr[12], -15)
+
+        return None
+
+    def parse(self):
+        try:
+            self.fp = open(self.fname, 'rb')
+            self.fsize = os.fstat(self.fp.fileno()).st_size
+
+            cur_pos = 0
+
+            while self.fsize != cur_pos:  # EOF?
+                sig = self.fp.read(4)
+                if sig == 'PK\x03\x04':
+                    zfr = self.__read_zipfilerecord()
+                    self.zipfilerecord.append(zfr)
+                elif sig == 'PK\x01\x02':
+                    zde = self.__read_zipdirentry()
+                    self.zipdirentry.append(zde)
+                elif sig == 'PK\x05\x05':
+                    zds = self.__read_zipdigitalsig()
+                    self.zipdigitalsig.append(zds)
+                elif sig == 'PK\x07\x08':
+                    zdd = self.__read_zipdatadescr()
+                    self.zipdatadescr.append(zdd)
+                elif sig == 'PK\x05\x06':
+                    zel = self.__read_zipendlocator()
+                    self.zipendlocator.append(zel)
+                    break
+                else:
+                    raise BadZipTagError('Tag is \'%s\'' % repr(sig))
+
+                cur_pos = self.fp.tell()
+
+            if self.fsize != self.fp.tell():
+                self.zipsize = self.fp.tell()  # 최종 ZipFile Size
+
+            return True
+        except (IOError, OSError) as e:
+            pass
+
+        return False
+
+    def __read_zipfilerecord(self):  # PK\x03\x04
+        data = self.fp.read(size_struct_zipfilerecord)
+        fr = struct.unpack(struct_zipfilerecord, data)
+        comp_len = fr[6]
+        fname_len = fr[8]
+        extra_len = fr[9]
+
+        fr = list(fr)
+        if fname_len > 0:
+            fname = self.fp.read(fname_len)
+            fr.append(fname)
+        else:
+            fr.append('')
+
+        if extra_len > 0:
+            fr.append(self.fp.read(extra_len))
+        else:
+            fr.append('')
+
+        if comp_len > 0:
+            fr.append(self.fp.read(comp_len))
+        else:
+            fr.append('')
+
+        return fr
+
+    def __read_zipdirentry(self):  # PK\x01\x02
+        data = self.fp.read(size_struct_zipdirentry)
+        de = struct.unpack(struct_zipdirentry, data)
+        fname_len = de[9]
+        extra_len = de[10]
+        comment_len = de[11]
+
+        de = list(de)
+        if fname_len > 0:
+            fname = self.fp.read(fname_len)
+            de.append(fname)
+        else:
+            de.append('')
+
+        if extra_len > 0:
+            de.append(self.fp.read(extra_len))
+        else:
+            de.append('')
+
+        if comment_len > 0:
+            de.append(self.fp.read(comment_len))
+        else:
+            de.append('')
+
+        return de
+
+    def __read_zipdigitalsig(self):  # PK\x05\x05
+        data = self.fp.read(size_struct_zipdigitalsig)
+        ds = struct.unpack(struct_zipdigitalsig, data)
+        data_len = ds[0]
+
+        ds = list(ds)
+        if data_len > 0:
+            ds.append(self.fp.read(data_len))
+        else:
+            ds.append('')
+
+        return ds
+
+    def __read_zipdatadescr(self):  # PK\x07\x08
+        data = self.fp.read(size_struct_zipdatadescr)
+        dd = struct.unpack(struct_zipdatadescr, data)
+
+        return list(dd)
+
+    def __read_zipendlocator(self):  # PK\x05\x06
+        data = self.fp.read(size_struct_zipendlocator)
+        el = struct.unpack(struct_zipendlocator, data)
+
+        return list(el)
+
+'''
+if __name__ == '__main__':
+    z = NZipFile('zip_attatch_data.bin')
+    z.parse()
+    for name in z.namelist():
+        print name
+
+    print z.read('docProps/app.xml')
+
+    z.close()
+'''
 
 # -------------------------------------------------------------------------
 # KavMain 클래스
@@ -37,7 +274,7 @@ class KavMain:
         info = dict()  # 사전형 변수 선언
 
         info['author'] = 'Kei Choi'  # 제작자
-        info['version'] = '1.0'  # 버전
+        info['version'] = '1.1'  # 버전
         info['title'] = 'Zip Archive Engine'  # 엔진 설명
         info['kmd_name'] = 'zip'  # 엔진 파일 이름
         info['engine_type'] = kernel.ARCHIVE_ENGINE  # 엔진 타입
@@ -77,26 +314,35 @@ class KavMain:
                 zfile = zipfile.ZipFile(filename)  # zip 파일 열기
                 names = zfile.namelist()
                 zfile.close()
+
+                # 파일 포맷은 ZIP이지만 특수 포맷인지를 한번 더 체크한다.
+                if names is not None:
+                    for name in names:
+                        n = name.lower()
+                        if n == 'classes.dex':
+                            ret['ff_apk'] = 'apk'
+                        elif n == 'xl/workbook.xml':
+                            ret['ff_ooxml'] = 'xlsx'
+                        elif n == 'word/document.xml':
+                            ret['ff_ooxml'] = 'docx'
+                        elif n == 'ppt/presentation.xml':
+                            ret['ff_ooxml'] = 'pptx'
+
+                    if len(ret) == 0:
+                        ret['ff_zip'] = 'zip'
             except zipfile.BadZipfile:
-                names = None
+                zfile = NZipFile(filename)
+                if zfile.parse():
+                    zsize = zfile.get_zipsize()
+                    fsize = os.path.getsize(filename)
+                    zfile.close()
 
-            # 파일 포맷은 ZIP이지만 특수 포맷인지를 한번 더 체크한다.
-            if names is not None:
-                for name in names:
-                    n = name.lower()
-                    if n == 'classes.dex':
-                        ret['ff_apk'] = 'apk'
-                    elif n == 'xl/workbook.xml':
-                        ret['ff_ooxml'] = 'xlsx'
-                    elif n == 'word/document.xml':
-                        ret['ff_ooxml'] = 'docx'
-                    elif n == 'ppt/presentation.xml':
-                        ret['ff_ooxml'] = 'pptx'
+                    if zsize < fsize and zsize != 0:
+                        # 파이썬 ZipFile로 해제되지 않는 파일 처리
+                        # zip 파일 뒤에 attach 된 데이터가 있으면 오류 발생
+                        ret['ff_attach_zip'] = (zsize, fsize - zsize)
 
-                if len(ret) == 0:
-                    ret['ff_zip'] = 'zip'
-
-                return ret
+            return ret
 
         return None
 
@@ -117,6 +363,10 @@ class KavMain:
             for name in zfile.namelist():
                 file_scan_list.append(['arc_zip', name])
             # zfile.close()
+        elif 'ff_attach_zip' in fileformat:
+            off, zsize = fileformat['ff_attach_zip']
+            file_scan_list.append(['arc_attach_zip:0:%d' % off, '#1'])
+            file_scan_list.append(['arc_attach_zip:%d:%d' % (off, zsize), '#2'])
 
         return file_scan_list
 
@@ -135,6 +385,15 @@ class KavMain:
                 return data
             except zipfile.BadZipfile:
                 pass
+        elif arc_engine_id.find('arc_attach_zip') != -1:
+            t = arc_engine_id.split(':')
+            off = int(t[1])
+            size = int(t[2])
+
+            with open(arc_name, 'rb') as fp:
+                fp.seek(off)
+                data = fp.read(size)
+                return data
 
         return None
 
