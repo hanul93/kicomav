0eaecde06c854cc94f825e277c05e12a4f729289
hanul93 <hanul93@gmail.com>
2020-01-02 10:10:45 +0900

    Added PluginsMain

--- Engine/plugins/kernel.py
+++ Engine/plugins/kernel.py
@@ -2,58 +2,66 @@
 # Made by Kei Choi(hanul93@naver.com)
 
 
-# 악성코드 상태값
-NOT_FOUND = 0  # 악성코드 찾지 못함
-INFECTED = 1  # 감염
-SUSPECT = 2  # 의심
-WARNING = 3  # 경고
-IDENTIFIED = 4  # 식별
-ERROR = 99  # 에러 메시지 처리
+# Malware scan result
+NOT_FOUND = 0
+INFECTED = 1
+SUSPECT = 2
+WARNING = 3
+IDENTIFIED = 4
+ERROR = 99
 
 
-# 악성코드 치료 방법
-MASTER_IGNORE = 0  # 현재 지원하지 않는 상태이므로 파일을 그대로 둘 것
-MASTER_PACK = 1  # 최상위 파일 압축(재구성), mkarc 함수 처리 가능
-MASTER_DELETE = 2  # 최상위 파일을 지울 것
+# How to Clean Malware for Compressed Files
+MASTER_IGNORE = 0  # it is not currently supported
+MASTER_PACK = 1  # Top-level file compression (reconstruction), can handle mkarc function
+MASTER_DELETE = 2  # Delete top-level file
 
 
-# 엔진의 타입
-ARCHIVE_ENGINE = 80  # 압축 해제 엔진
+# Engine type
+ARCHIVE_ENGINE = 80
 
 
 # -------------------------------------------------------------------------
-# KavMain 클래스
+# class PluginsMain
 # -------------------------------------------------------------------------
-class KavMain:
+class PluginsMain(object):
     # ---------------------------------------------------------------------
-    # init(self, plugins_path)
-    # 플러그인 엔진을 초기화 한다.
-    # 인력값 : plugins_path - 플러그인 엔진의 위치
-    #         verbose      - 디버그 모드 (True or False)
-    # 리턴값 : 0 - 성공, 0 이외의 값 - 실패
+    # init(self, plugins_path, verbose)
+    # Initialize the plug-in engine
+    # input  : plugins_path - Location of the plug-in engine
+    #          verbose      - verbose (True or False)
+    # return : 0 - success, Nonzero - fail
     # ---------------------------------------------------------------------
-    def init(self, plugins_path, verbose=False):  # 플러그인 엔진 초기화
-        return 0  # 플러그인 엔진 초기화 성공
+    def init(self, plugins_path, verbose=False):
+        return 0
 
     # ---------------------------------------------------------------------
     # uninit(self)
-    # 플러그인 엔진을 종료한다.
-    # 리턴값 : 0 - 성공, 0 이외의 값 - 실패
+    # Quit the plug-in engine
+    # return : 0 - success, Nonzero - fail
     # ---------------------------------------------------------------------
-    def uninit(self):  # 플러그인 엔진 종료
-        return 0  # 플러그인 엔진 종료 성공
+    def uninit(self):
+        return 0
 
     # ---------------------------------------------------------------------
     # getinfo(self)
-    # 플러그인 엔진의 주요 정보를 알려준다. (제작자, 버전, ...)
-    # 리턴값 : 플러그인 엔진 정보
+    # Provides information about the plug-in engine. (author, version, ...)
+    # return : Plug-in information
     # ---------------------------------------------------------------------
-    def getinfo(self):  # 플러그인 엔진의 주요 정보
-        info = dict()  # 사전형 변수 선언
+    def getinfo(self):
+        return dict()
 
-        info['author'] = 'Kei Choi'  # 제작자
-        info['version'] = '1.0'  # 버전
-        info['title'] = 'KicomAV Kernel'  # 엔진 설명
-        info['kmd_name'] = 'kernel'  # 엔진 파일 이름
+
+# -------------------------------------------------------------------------
+# class KavMain
+# -------------------------------------------------------------------------
+class KavMain(PluginsMain):
+    def getinfo(self):
+        info = dict()
+
+        info['author'] = 'Kei Choi'
+        info['version'] = '1.0'
+        info['title'] = 'KicomAV Kernel'  # Plug-in engine description
+        info['kmd_name'] = 'kernel'  # filename of Plug-in engine
 
         return info
